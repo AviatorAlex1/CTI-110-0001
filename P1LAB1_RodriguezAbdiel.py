220b9096432031e9f4bdcb43692a6732836fc848
AviatorAlex1 <rodrigua4692@student.faytechcc.edu>
2025-02-10 11:32:25 -0500

    P1LAB1

---
+++ P1LAB1_RodriguezAbdiel.py
@@ -0,0 +1,14 @@
+ # Abdiel Rodriguez
+ # 2/10/2025
+ # P1LAB1
+ # Getting User input and displaying the input as a print function.
+
+# Get name and address from User
+
+# Variables 
+#------------------------------------------------------------------
+User_FirstName = input("Enter your First Name: ")
+User_LastName = input("Enter your Last Name: ")
+
+# Get User First Name
+print("Hello,",User_FirstName,User_LastName, ",Welcome to CTI-110-0001")
